0f2b8cdf70d8f1bb451507d76eaf584c8be286f7
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-21 08:54:37 +0200

    cambiamenti

--- ConvenienzaNegozzi/Program.cs
+++ ConvenienzaNegozzi/Program.cs
@@ -6,7 +6,28 @@ namespace ConvenienzaNegozzi
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("inserisci il prezzo del primo prodotto");
+            double p1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("inserisci il prezzo del secondo prodotto");
+            double p2 = double.Parse(Console.ReadLine());
+            double sconto;
+            double scontato;
+            double totale;
+            if (p1 > p2)
+            {
+                sconto = p2 / 100 * 50;
+                scontato = p2 - sconto;
+                totale = p1 + scontato;
+                Console.WriteLine($"lo sconto è di {sconto} euro e il prezzo da pagare è {totale} euro");
+            }
+            else
+            {
+                sconto = p1 / 100 * 50;
+                scontato = p1 - sconto;
+                totale = p2 + scontato;
+                Console.WriteLine($"lo sconto è di {sconto} euro e il prezzo da pagare è {totale} euro");
+            }
+            Console.ReadLine();
         }
     }
 }
