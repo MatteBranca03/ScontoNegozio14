70589414768404804be9abb774148e8c0eb76496
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-21 08:27:16 +0200

    Add project files.

---
+++ ConvenienzaNegozzi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConvenienzaNegozzi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
